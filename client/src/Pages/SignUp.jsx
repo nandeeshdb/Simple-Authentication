36f42ea0eb38e015b7d8b22c0b2b841d9fa0ad38
nandeeshdb <nandeeshbasavarajaiah@gmail.com>
2023-10-29 01:09:43 +0530

    Complete Sign-Up Page Desiging

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function SignUp() {
   return (
     <div className='p-20 max-w-lg mx-auto'>
-      <h1 className='text-center font-semibold text-3xl my-8'>Sign Up</h1>
+      <h1 className='text-center font-semibold text-3xl my-7'>Sign Up</h1>
       <form className='flex flex-col gap-4'>
         <input type="text" placeholder='Username' id='username' className='bg-slate-100 p-2 rounded-lg'/>
         <input type="email" placeholder='Email' id='email' className='bg-slate-100 p-2 rounded-lg'/>
