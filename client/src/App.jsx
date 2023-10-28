71d159443db462d52f1b16332602a3779cb9e5d5
nandeeshdb <nandeeshbasavarajaiah@gmail.com>
2023-10-28 17:22:07 +0530

    Header Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
-import {BrowserRouter,Routes,Router} from 'react-router-dom';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import Home from './pages/Home';
+import Header from './components/Header';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
@@ -8,6 +9,7 @@ import SignUp from './pages/SignUp';
 export default function App() {
   return (
    <BrowserRouter>
+   <Header />
    <Routes>
     <Route path='/' element={<Home />}/>
     <Route path='/about' element={<About />}/>
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+function Header() {
+  return (
+    <div className='bg-slate-300'>
+        <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+           <Link to='/'> <h1 className='font-bold'>Simple Auth</h1></Link>
+           <ul className='flex gap-3' >
+            <Link to='/'><li>Home</li></Link>
+            <Link  to='about'><li>About</li></Link>
+            <Link to='sign-in'><li>Sign In</li></Link>
+           </ul>
+        </div>
+
+
+
+    </div>
+  )
+}
+
+export default Header
