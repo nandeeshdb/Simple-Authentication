912c7c2797c55958dbbc3898125f684c3df247b7
nandeeshdb <nandeeshbasavarajaiah@gmail.com>
2023-10-30 17:21:47 +0530

    Updating header component and making profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import About from './pages/About';
 import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import PrivateRoute from './components/PrivateRoute';
  
 export default function App() {
   return (
@@ -13,7 +14,9 @@ export default function App() {
    <Routes>
     <Route path='/' element={<Home />}/>
     <Route path='/about' element={<About />}/>
-    <Route path='/profile' element={<Profile />}/>
+    <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+    </Route>
     <Route path='/sign-in' element={<SignIn />}/>
     <Route path='/sign-up' element={<SignUp />}/>
    </Routes>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {useSelector} from 'react-redux'
 
 function Header() {
+  const {currentUser} = useSelector((state)=>state.user)
   return (
     <div className='bg-slate-300'>
         <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -9,7 +11,14 @@ function Header() {
            <ul className='flex gap-3' >
             <Link to='/'><li>Home</li></Link>
             <Link  to='about'><li>About</li></Link>
-            <Link to='sign-in'><li>Sign In</li></Link>
+
+            <Link to='/profile'>
+
+            {currentUser?(
+              <img src={currentUser.profilePicture} alt="no" className='w-7 h-7 rounded-full object-cover'/>
+            ):(<li>Sign In</li>)}
+            </Link>
+            
            </ul>
         </div>
 
