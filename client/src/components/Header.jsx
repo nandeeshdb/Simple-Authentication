39698a84247c3b1f1a3c3bc99a1c6c1b7ac3726f
nandeeshdb <nandeeshbasavarajaiah@gmail.com>
2023-11-12 01:10:15 +0530

    complete header  search functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,19 +1,20 @@
-import { FaSearch } from 'react-icons/fa';
-import { Link, useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
+import {Link,useNavigate} from 'react-router-dom'
+import {useSelector} from 'react-redux'
+import {FaSearch} from 'react-icons/fa'
 
-export default function Header() {
-  const { currentUser } = useSelector((state) => state.user);
-  const [searchTerm, setSearchTerm] = useState('');
-  const navigate = useNavigate();
-  const handleSubmit = (e) => {
+function Header() {
+  const {currentUser} = useSelector((state)=>state.user)
+  const[searchTerm,setSearchTerm] = useState('')
+  const navigate =  useNavigate()
+
+  const handleSubmit=(e)=>{
     e.preventDefault();
     const urlParams = new URLSearchParams(window.location.search);
-    urlParams.set('searchTerm', searchTerm);
+    urlParams.set('searchTerm',searchTerm)
     const searchQuery = urlParams.toString();
-    navigate(`/search?${searchQuery}`);
-  };
+    navigate(`/search?/${searchQuery}`)
+  }
 
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
@@ -23,53 +24,39 @@ export default function Header() {
     }
   }, [location.search]);
   return (
-    <header className='bg-slate-200 shadow-md'>
-      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-        <Link to='/'>
-          <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Sahand</span>
-            <span className='text-slate-700'>Estate</span>
-          </h1>
-        </Link>
-        <form
-          onSubmit={handleSubmit}
-          className='bg-slate-100 p-3 rounded-lg flex items-center'
-        >
-          <input
+    <div className='bg-slate-300'>
+        <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+           <Link to='/'> <h1 className='font-bold'>Simple Auth</h1></Link>
+           <form onSubmit={handleSubmit} className='bg-slate-100 p-2 rounded-lg flex items-center'>
+           <input
             type='text'
-            placeholder='Search...'
-            className='bg-transparent focus:outline-none w-24 sm:w-64'
+            placeholder='search...'
+            className='bg-transparent focus:outline-none  w-24 sm:w-64 '
             value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-          <button>
-            <FaSearch className='text-slate-600' />
-          </button>
-        </form>
-        <ul className='flex gap-4'>
-          <Link to='/'>
-            <li className='hidden sm:inline text-slate-700 hover:underline'>
-              Home
-            </li>
-          </Link>
-          <Link to='/about'>
-            <li className='hidden sm:inline text-slate-700 hover:underline'>
-              About
-            </li>
-          </Link>
-          <Link to='/profile'>
-            {currentUser ? (
-              <img
-                className='rounded-full h-7 w-7 object-cover'
-                src={currentUser.avatar}
-                alt='profile'
-              />
-            ) : (
-              <li className=' text-slate-700 hover:underline'> Sign in</li>
-            )}
-          </Link>
-        </ul>
-      </div>
-    </header>
-  );
+            onChange={(e)=>setSearchTerm(e.target.value)}
+            
+           />
+           <button><FaSearch className='text-slate-600'/></button>
+           
+           </form>
+           <ul className='flex gap-3' >
+            <Link to='/'><li>Home</li></Link>
+            <Link  to='about'><li>About</li></Link>
+
+            <Link to='/profile'>
+
+            {currentUser?(
+              <img src={currentUser.profilePicture} alt="no" className='w-7 h-7 rounded-full object-cover'/>
+            ):(<li>Sign In</li>)}
+            </Link>
+            
+           </ul>
+        </div>
+
+
+
+    </div>
+  )
 }
+
+export default Header
