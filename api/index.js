8e033a3f5b0c4282ccc42b31c19eaaa979845703
nandeeshdb <nandeeshbasavarajaiah@gmail.com>
2023-11-12 13:37:25 +0530

    fixing small bug

--- api/index.js
+++ api/index.js
@@ -39,7 +39,7 @@ app.use('/api/listing',listingRoutes)
 
 app.use(express.static(path.join(__dirname,'/client/dist')))
 
-app.get(*,(req,res)=>{
+app.get('*',(req,res)=>{
     res.sendFile(path.join(__dirname,'client','dist','index.html'))
 })
 
