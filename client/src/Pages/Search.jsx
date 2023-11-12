92204f25fd085bbc8e12f0c811dd528950fe9a04
nandeeshdb <nandeeshbasavarajaiah@gmail.com>
2023-11-12 10:58:34 +0530

    adding onChange and onSubmit functionality to the search page

--- client/src/Pages/Search.jsx
+++ client/src/Pages/Search.jsx
@@ -1,10 +1,101 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {useNavigate} from 'react-router-dom'
 
 function Search() {
+    const[slideBarData,setSlideBarData]=useState({
+        searchTerm:'',
+        type:'all',
+        parking:false,
+        furnished:false,
+        offer:false,
+        sort:'created_at',
+        order:'desc'
+    })
+
+    const navigate = useNavigate()
+
+    const[listing,setListing] =useState([])
+    console.log(listing)
+
+
+    useEffect(()=>{
+        const urlParams = new URLSearchParams(location.search)
+        const searchTermFromUrl = urlParams.get('searchTerm')
+        const typeFromUrl = urlParams.get('type')
+        const parkingFromUrl = urlParams.get('parking')
+        const furnishedFromUrl = urlParams.get('furnished')
+        const offerFromUrl = urlParams.get('offer')
+        const sortFromUrl = urlParams.get('sort')
+        const orderFromUrl = urlParams.get('order')
+
+        if(searchTermFromUrl||typeFromUrl||parkingFromUrl||furnishedFromUrl||offerFromUrl||sortFromUrl||orderFromUrl){
+            setSlideBarData({
+                searchTerm:searchTermFromUrl||'',
+                type:typeFromUrl||'all',
+                parking:parkingFromUrl==='true' ? true : false,
+                furnished:furnishedFromUrl==='true' ? true : false,
+                offer:offerFromUrl==='true' ? true : false,
+                sort:sortFromUrl || 'created_at',
+                order:orderFromUrl || 'desc'
+            })
+        }
+
+        const fetchListing = async()=>{
+            const searchQuery = urlParams.toString() 
+            const res = await fetch(`/api/listing/get?${searchQuery}`)
+            const data = await res.json();
+            setListing(data)
+
+
+        }
+
+        fetchListing();
+    
+    },[location.search])
+
+  
+
+
+
+    const handleChange =(e)=>{
+
+        if(e.target.id==='all'||e.target.id==='rent'||e.target.id==='sale'){
+            setSlideBarData({...slideBarData,type:e.target.id})
+        }
+        if(e.target.id==='searchTerm'){
+            setSlideBarData({...slideBarData,searchTerm:e.target.value})
+        }
+
+        if(e.target.id==='parking' || e.target.id==='furnished' || e.target.id==='offer'){
+            setSlideBarData({...slideBarData, 
+                [e.target.id]:e.target.checked || e.target.checked==='true' ? true : false })
+        }
+        if(e.target.id==='sort_order'){
+            const sort = e.target.value.split('_')[0] || 'created_at'
+            const order = e.target.value.split('_')[1] || 'desc'
+            setSlideBarData({...slideBarData,sort,order})
+        }
+
+    }
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        const urlParams = new URLSearchParams();
+        urlParams.set('searchTerm', slideBarData.searchTerm)
+        urlParams.set('type', slideBarData.type)
+        urlParams.set('parking', slideBarData.parking)
+        urlParams.set('furnished', slideBarData.furnished)
+        urlParams.set('offer', slideBarData.offer)
+        urlParams.set('sort', slideBarData.sort)
+        urlParams.set('order', slideBarData.order)
+        const searchQuery = urlParams.toString()
+        navigate(`/search?${searchQuery}`)
+    }
+
   return (
     <div className='flex flex-col md:flex-row'>
         <div className=' p-8 border-b-2 md:border-r-2 md:min-h-screen'>
-                <form className='flex flex-col gap-8'>
+                <form className='flex flex-col gap-8' onSubmit={handleSubmit}>
                     <div className='flex gap-3 items-center'>
                         <label className='whitespace-nowrap font-semibold' >Search Term:</label>
                         <input
@@ -12,45 +103,47 @@ function Search() {
                         id='searchTerm'
                         placeholder='search'
                         className='border p-3 w-full rounded-lg ' 
+                        onChange={handleChange}
+                        value={slideBarData.searchTerm}
                         />
                     </div>
                     <div className='flex gap-2 items-center flex-wrap '>
                         <label className='font-semibold'>Type:</label>
                         <div className='flex gap-2 '>
-                            <input type='checkbox' id='all' className='w-5'/>
+                            <input type='checkbox' id='all' className='w-5' onChange={handleChange} checked={slideBarData.type==='all'}/>
                             <span>Rent & Sale</span>
                         </div>
                         <div className='flex gap-2 '>
-                            <input type='checkbox' id='rent' className='w-5'/>
+                            <input type='checkbox' id='rent' className='w-5' onChange={handleChange} checked={slideBarData.type==='rent'}/>
                             <span>Rent </span>
                         </div>
                         <div className='flex gap-2 '>
-                            <input type='checkbox' id='sale' className='w-5'/>
+                            <input type='checkbox' id='sale' className='w-5' onChange={handleChange} checked={slideBarData.type==='sale'}/>
                             <span> Sale</span>
                         </div>
                         <div className='flex gap-2 '>
-                            <input type='checkbox' id='offer' className='w-5'/>
+                            <input type='checkbox' id='offer' className='w-5' onChange={handleChange} checked={slideBarData.offer===true}/>
                             <span>Offer</span>
                         </div>
                     </div>
                     <div className='flex gap-2 items-center flex-wrap '>
                         <label className='font-semibold'>Amedities:</label>
                         <div className='flex gap-2 '>
-                            <input type='checkbox' id='parking' className='w-5'/>
+                            <input type='checkbox' id='parking' className='w-5' onChange={handleChange} checked={slideBarData.parking===true} />
                             <span>Parking</span>
                         </div>
                         <div className='flex gap-2 '>
-                            <input type='checkbox' id='furnished' className='w-5'/>
+                            <input type='checkbox' id='furnished' className='w-5' onChange={handleChange} checked={slideBarData.furnished===true}/>
                             <span>Furnished </span>
                         </div>
                     </div>
                     <div className='flex items-center gap-2'>
                         <label className='font-semibold'>Sort: </label>
-                        <select id='sort_order' className='border p-3 rounded-lg'>
-                            <option>Price high to low</option>
-                            <option>Price low to high</option>
-                            <option>Price latest</option>
-                            <option>Price oldest</option>
+                        <select id='sort_order' className='border p-3 rounded-lg' onChange={handleChange} defaultValue={'created_at_desc'}>
+                            <option value='regularPrice_desc'>Price high to low</option>
+                            <option value='regularPrice_asc'>Price low to high</option>
+                            <option value='createdAt_desc'>Price latest</option>
+                            <option value='createdAt_asc'>Price oldest</option>
 
                         </select>
                     </div>
